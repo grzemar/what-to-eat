898bde29cb7f2273f39a6bcc4f7146177a92d9be
Tomasz Gieniusz <tomasz.gieniusz@gmail.com>
2012-04-10 00:07:24 +0200

    android hello world

---
+++ WhatToEat/src/ynd/whattoeat/Utils.java
@@ -0,0 +1,47 @@
+package ynd.whattoeat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.Toast;
+
+public class Utils {
+	public static String getFromURL(String url) throws IOException {
+		URL urll = new URL(url);
+		BufferedReader in = new BufferedReader(new InputStreamReader(urll.openStream()));
+
+		String all = "";
+
+		String s;
+		while ((s = in.readLine()) != null)
+			all += s + "\n";
+
+		return all;
+	}
+
+	public static Bitmap getBitmapFromURL(String src) {
+		try {
+			URL url = new URL(src);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoInput(true);
+			connection.connect();
+			InputStream input = connection.getInputStream();
+			Bitmap myBitmap = BitmapFactory.decodeStream(input);
+			return myBitmap;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void connectionErrorToast(Context context) {
+		Toast.makeText(context, "Couldn't connect to the server.\nTry again later", Toast.LENGTH_LONG).show();
+	}
+}

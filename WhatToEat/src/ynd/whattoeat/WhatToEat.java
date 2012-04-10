5b8e2c837d0db219899f23c759a8bc59b2f45032
Shadarath <bart.jankowski@gmail.com>
2012-04-10 22:34:31 +0200

    kilka potraw

--- WhatToEat/src/ynd/whattoeat/WhatToEat.java
+++ WhatToEat/src/ynd/whattoeat/WhatToEat.java
@@ -3,7 +3,7 @@ package ynd.whattoeat;
 import java.util.Random;
 
 public class WhatToEat {
-	private static String[] dishes = new String[] { "Pizza", "Ice creams" };
+	private static String[] dishes = new String[] { "Pizza", "Ice creams", "Chocolate", "Apple Pie", "Spaghetti",  };
 
 	public static String whatToEat() {
 		return dishes[new Random().nextInt(dishes.length)];
